bffb110fc20e865a0e7fe9971d76370bc089e1a5
Brian Olsen <brian@maven-group.org>
2017-09-24 15:12:10 +0200

    Add debug to decrypt script

--- Scripts/decrypt.bash
+++ Scripts/decrypt.bash
@@ -1,4 +1,5 @@
 set -o errexit
+set -x
 
 if [[ ( -n "$encrypted_8e7577110561_key" ) -a ( -n "$encrypted_8e7577110561_iv" ) ]]; then
   openssl aes-256-cbc -K $encrypted_8e7577110561_key -iv $encrypted_8e7577110561_iv \
