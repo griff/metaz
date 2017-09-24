c8c6a402793ce71b97bc094efd57a803e25ab962
Brian Olsen <brian@maven-group.org>
2017-09-24 15:20:26 +0200

    Try to fix conditional expression in decrypt

--- Scripts/decrypt.bash
+++ Scripts/decrypt.bash
@@ -1,7 +1,9 @@
 set -o errexit
 set -x
 
-if [[ ( -n "$encrypted_8e7577110561_key" ) -a ( -n "$encrypted_8e7577110561_iv" ) ]]; then
-  openssl aes-256-cbc -K $encrypted_8e7577110561_key -iv $encrypted_8e7577110561_iv \
-    -in sparkle_private.pem.enc -out sparkle_private.pem -d
+if [[ -n "$encrypted_8e7577110561_key" ]]; then
+  if [[ -n "$encrypted_8e7577110561_iv" ]]; then
+    openssl aes-256-cbc -K $encrypted_8e7577110561_key -iv $encrypted_8e7577110561_iv \
+      -in sparkle_private.pem.enc -out sparkle_private.pem -d
+  fi
 fi
