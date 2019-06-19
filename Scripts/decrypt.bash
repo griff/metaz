436f898a052641a1525f7f7f4c767b7bf8191057
Brian Olsen <brian@maven-group.org>
2019-06-19 13:20:20 +0200

    Make new encryption key

--- Scripts/decrypt.bash
+++ Scripts/decrypt.bash
@@ -1,9 +1,9 @@
 set -o errexit
 set -x
 
-if [[ -n "$encrypted_8e7577110561_key" ]]; then
-  if [[ -n "$encrypted_8e7577110561_iv" ]]; then
-    openssl aes-256-cbc -K $encrypted_8e7577110561_key -iv $encrypted_8e7577110561_iv \
+if [[ -n "$encrypted_407c52b0da85_key" ]]; then
+  if [[ -n "$encrypted_407c52b0da85_iv" ]]; then
+    openssl aes-256-cbc -K $encrypted_407c52b0da85_key -iv $encrypted_407c52b0da85_iv \
       -in sparkle_private.pem.enc -out sparkle_private.pem -d
   fi
 fi
