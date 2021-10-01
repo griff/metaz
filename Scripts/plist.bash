c3ebca70f92325f630895fb51541e6385a955b8c
Brian Olsen <brian@maven-group.org>
2021-10-01 03:22:38 +0200

    Correcting release number

--- Scripts/plist.bash
+++ Scripts/plist.bash
@@ -17,17 +17,22 @@ minor="$(echo $release | cut -d . -f 2 -)"
 while git show-ref --tags --quiet --verify -- "refs/tags/v${major}.${minor}" ; do
   ((minor = $minor + 1))
 done
-if [[ $GITHUB_REF != refs/tags/* ]]; then
-  #number="$(git rev-list --count $GITHUB_REF)"
-  ((number = $GITHUB_RUN_NUMBER + 116))
-  release="${major}.${minor}.beta-$number"
-else
-  ((minors = $minor - 1))
-  if [[ "$GITHUB_REF" == "refs/tags/v${major}.${minors}" ]]; then
-    release="${major}.${minors}"
+if [[ -n "$GITHUB_REF" ]]; then
+  if [[ $GITHUB_REF != refs/tags/* ]]; then
+    #number="$(git rev-list --count $GITHUB_REF)"
+    ((number = $GITHUB_RUN_NUMBER + 116))
+    release="${major}.${minor}.beta-$number"
   else
-    release="${major}.${minor}"
+    ((minors = $minor - 1))
+    if [[ "$GITHUB_REF" == "refs/tags/v${major}.${minors}" ]]; then
+      release="${major}.${minors}"
+    else
+      release=${GITHUB_REF#refs/tags/v}
+      #release="${major}.${minor}"
+    fi
   fi
+else
+  release="${major}.${minor}"
 fi
 
 if git show-ref --tags --quiet --verify -- "refs/tags/v$release"; then
